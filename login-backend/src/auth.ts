5a74ffabd530c388937ca31b53c1c426b20eaa17
jefferson1104 <jefferson1104junior@gmail.com>
2021-10-22 11:16:58 -0300

    realizando refresh token

--- login-backend/src/auth.ts
+++ login-backend/src/auth.ts
@@ -6,7 +6,7 @@ import { createRefreshToken } from './database';
 export function generateJwtAndRefreshToken(email: string, payload: object = {}) {
   const token = jwt.sign(payload, auth.secret, {
     subject: email,
-    expiresIn: 60, // Depois colocar 15 minutos (60 * 15)
+    expiresIn: 10, // Depois colocar 15 minutos (60 * 15)
   });
 
   const refreshToken = createRefreshToken(email)
