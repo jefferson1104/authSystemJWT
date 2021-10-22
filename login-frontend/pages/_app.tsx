37a8984996fe53109e34f5042f89ce74620be22c
jefferson1104 <jefferson1104junior@gmail.com>
2021-10-22 02:59:35 -0300

    contexto de autenticação

--- login-frontend/pages/_app.tsx
+++ login-frontend/pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AuthProvider } from '../contexts/AuthContext'
+
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 export default MyApp
--- login-frontend/pages/index.tsx
+++ login-frontend/pages/index.tsx
@@ -1,9 +1,25 @@
-import type { NextPage } from 'next'
+import { FormEvent, useContext, useState } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = {email, password}
+
+    await signIn(data);
+  }
+
   return (
-    <h1>Login system</h1>
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button type="submit">Entrar</button>
+    </form>
   )
 }
-
-export default Home
